0e6d36cccbc3be4cb26b293a2a825bc1b44823a5
elm06542@gmail.com <elm06542@gmail.com>
2023-12-08 19:34:02 +0200

    animation load

---
+++ main.js
@@ -0,0 +1,8 @@
+function hide(){
+  document.querySelector(".load").remove();
+  document.querySelector("img").style.display="block";
+  document.querySelector(".head").style.display="block";
+  document.querySelector(".pragh").style.display="block"; 
+}
+setTimeout(hide,1000);
+  
